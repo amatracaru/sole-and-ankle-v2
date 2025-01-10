503c48abecb79acbd0e17ec04a62465a4f851fc6
Andrei Matracaru <andrei.matracaru@gmail.com>
2025-01-10 21:41:23 +0100

    Ex 2: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,11 +27,22 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
+  position: relative;
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 72px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav` 
+  position: absolute;
+  left: 50vh;
+  display: flex;
+  align-items: center;
+  gap: 48px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
